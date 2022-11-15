16ba97fb4811aba89af65281382e5650d655f4db
tapok <trump6009@gmail.com>
2022-11-15 19:54:43 +0300

    rails-test-blog-6-rspec_controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -29,6 +28,8 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
