e7a4c5de225bdff136e78bca180be2a2b090c2ba
tapok <trump6009@gmail.com>
2022-11-24 22:54:10 +0300

    rails-test-blog-9-edits

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ApplicationRecord
-  validates :title, :body, presence:true
-  has_one_attached :post_image
+  validates_presence_of :title, :body
+  validates_uniqueness_of :title
+  has_one_attached :image
   has_many :comments, dependent: :destroy
+
+  scope :by_user, -> (user) {
+    where(:user_id => user)
+  }
+
+  paginates_per 15
 end
